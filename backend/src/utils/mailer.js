ab04237e26e22b264631f159fd1306c9a0ef482b
angelacarvajal414-code <https://github.com/angelacarvajal414-code/mimmos.git>
2026-09-09 21:42:45 -0500

    brevo conectado correctamemnte

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,11 +1,18 @@
-import nodemailer from 'nodemailer';
+import brevo from '@getbrevo/brevo';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+const apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
+
+const transporter = {
+  sendMail: async ({ from, to, subject, html }) => {
+    const correo = new brevo.SendSmtpEmail();
+    correo.sender = { email: from || process.env.EMAIL_USER, name: 'FitZone' };
+    correo.to = [{ email: to }];
+    correo.subject = subject;
+    correo.htmlContent = html;
+
+    return await apiInstance.sendTransacEmail(correo);
   }
-});
+};
 
 export default transporter;
